8737a9bc9e12cff9d1102338e73d01157b2aa669
Joelle <joellekjx@gmail.com>
2021-11-22 01:36:19 +0800

    update init.sql

--- Homework 2/script/init.sql
+++ Homework 2/script/init.sql
@@ -25,3 +25,29 @@ INSERT INTO public.users(
 	('2', 'user2', '1133', 22, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	('3', 'user3', '1133', 32, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	('4', 'user4', '1133', 42, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- Table: public.groups
+
+DROP TABLE public.groups;
+
+CREATE TABLE IF NOT EXISTS public.groups
+(
+    id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    permissions character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT groups_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.groups
+    OWNER to postgres;
+
+INSERT INTO public.groups(
+	id, name, permissions, "createdAt", "updatedAt")
+	VALUES ('1', 'group1', ['READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('2', 'group2', ['READ', 'WRITE', 'DELETE', 'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('3', 'group3', ['READ',  'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('4', 'group4', ['READ'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
