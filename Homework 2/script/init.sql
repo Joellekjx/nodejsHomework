980d1cc1292282a03d043f463904a5f564a6cd09
Joelle <joellekjx@gmail.com>
2021-11-22 01:36:35 +0800

    update init.sql

--- Homework 2/script/init.sql
+++ Homework 2/script/init.sql
@@ -1,6 +1,10 @@
--- Table: public.users
+-- Drop Tables
 
+DROP TABLE public."userGroups";
+DROP TABLE public.groups;
 DROP TABLE public.users;
+
+-- Table: public.users
 
 CREATE TABLE IF NOT EXISTS public.users
 (
@@ -19,16 +23,9 @@ TABLESPACE pg_default;
 ALTER TABLE public.users
     OWNER to postgres;
 
-INSERT INTO public.users(
-	id, login, password, age, "isDeleted", "createdAt", "updatedAt")
-	VALUES ('1', 'user1', '1133', 12, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('2', 'user2', '1133', 22, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('3', 'user3', '1133', 32, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('4', 'user4', '1133', 42, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- Table: public.groups
 
-DROP TABLE public.groups;
 
 CREATE TABLE IF NOT EXISTS public.groups
 (
@@ -45,9 +42,53 @@ TABLESPACE pg_default;
 ALTER TABLE public.groups
     OWNER to postgres;
 
+
+
+-- Table: public.userGroups
+
+
+CREATE TABLE IF NOT EXISTS public."userGroups"
+(
+    id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "userId" character varying(255) COLLATE pg_catalog."default",
+    "groupId" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT "userGroups_pkey" PRIMARY KEY (id),
+    CONSTRAINT "userGroups_groupId_fkey" FOREIGN KEY ("groupId")
+        REFERENCES public.groups (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT "userGroups_userId_fkey" FOREIGN KEY ("userId")
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."userGroups"
+    OWNER to postgres;
+
+
+INSERT INTO public.users(
+	id, login, password, age, "isDeleted", "createdAt", "updatedAt")
+	VALUES ('1', 'user1', '1133', 12, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('2', 'user2', '1133', 22, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('3', 'user3', '1133', 32, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('4', 'user4', '1133', 42, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 INSERT INTO public.groups(
 	id, name, permissions, "createdAt", "updatedAt")
-	VALUES ('1', 'group1', ['READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('2', 'group2', ['READ', 'WRITE', 'DELETE', 'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('3', 'group3', ['READ',  'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	('4', 'group4', ['READ'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	VALUES ('1', 'group1', ARRAY ['READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('2', 'group2', ARRAY ['READ', 'WRITE', 'DELETE', 'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('3', 'group3', ARRAY ['READ', 'SHARE'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	('4', 'group4', ARRAY ['READ'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO public."userGroups"(
+	id,  "userId", "groupId", "createdAt", "updatedAt")
+	VALUES ('1', '1', '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('2', '2', '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('3', '3', '2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('4', '4', '4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('5', '4', '3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
